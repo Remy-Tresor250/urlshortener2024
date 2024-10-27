6b8927f1d689ed60f4d652e07e207668d7954edc
mwizerwa77 <mwizerwa77@gmail.com>
2024-10-27 06:43:30 +0200

    new repo

--- src/test/java/com/takeHome/stanley/urlShortenerApp/services/impl/ShortUrlServiceImplTest.java
+++ src/test/java/com/takeHome/stanley/urlShortenerApp/services/impl/ShortUrlServiceImplTest.java
@@ -17,7 +17,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
-class ShortUrlServiceImplTest {
+public class ShortUrlServiceImplTest {
     @Mock
     private IShortUrlRepository repository;
 
